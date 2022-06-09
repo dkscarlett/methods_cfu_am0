63a5fac25931ae3ecada087a3ee4c59a253f6a3e
Dylan Scarlett <dylscar27@gmail.com>
2022-06-09 13:21:03 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -8,20 +8,32 @@
 "Hello World".downcase
 
 # YOU DO the rest:
-
+# The include? method is called on the string "Hello World"
+# One argument is passed ("Hello")
+# The return is true
 
 "Hello World".include?("Hello")
 
+# The end_with? method is called on the string "Hello World"
+# One argument is passed ("Hello")
+# The return is false
 
 "Hello World".end_with?("Hello")
 
+# The method end_with? is called on the string ("Hello World")
+# One argument is passed ("rld")
+# The returbn is true
 
 "Hello World".end_with?("rld")
 
+# The even? method is called on to the integer 12
+# No arguments are passed
+# The return is true
 
 12.even?
 
 
+# The nest method is called on to the integer 18
+# No arguments are passed
+# The return is 19
 18.next
-
-
